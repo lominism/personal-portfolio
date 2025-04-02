4ebf5eff2fbecd6e11586b1494ef273dfcbb84e7
lominism <lominism@gmail.com>
2025-04-02 16:34:07 +0700

    Finished Projects Section

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,8 +1,12 @@
 export const ProjectsCard = ({ link, image, title }) => {
   return (
-    <a href={link}>
+    <a href={link} target="_blank" rel="noopener noreferrer">
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 hover:-translate-y-1 transition-all">
-        <img className="rounded-t-lg" src={image} alt="" />
+        <img
+          className="rounded-t-lg object-cover h-52 w-full block mx-auto"
+          src={image}
+          alt=""
+        />
         <div className="p-2 h-20">
           <h5 className="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {title}
